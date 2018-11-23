c9841c92ac22c5ed49cc7c4f68249eb7f8fc2624
Victor Sharamet <sharamet@gmail.com>
2018-11-23 23:16:57 +0300

    mysql lessons

--- mysql-users.php
+++ mysql-users.php
@@ -6,7 +6,17 @@ if ( mysqli_connect_error() ) {
 	die ("Ошибка подключения к базе данных.");
 }
 
-$query = "SELECT * FROM `users`";
+// $query = "SELECT * FROM `users`";
+// $query = "SELECT * FROM `users` WHERE `id` = 1";
+// $query = "SELECT * FROM `users` WHERE `email` = 'joker@hotmail.com'";
+// $query = "SELECT * FROM `users` WHERE `email` = 'joker@hotmail.com'";
+// $query = "SELECT * FROM `users` WHERE `email` LIKE 'joker@hotmail.com'";
+// $query = "SELECT * FROM `users` WHERE `email` LIKE '%@gmail.com'";
+// $query = "SELECT * FROM `users` WHERE `email` LIKE '%man%'";
+// $query = "SELECT * FROM `users` WHERE `id` > 2";
+// $query = "SELECT * FROM `users` WHERE `id` >= 2";
+$query = "SELECT * FROM `users` WHERE `id` >= 2 AND `email` LIKE '%gmail.com'";
+
 
 if ( $result = mysqli_query($link, $query) ) {
 	
